7385a7c6e373aaf4e7a275878ae9d911a3cb82e3
Sorel <sodeve619@gmail.com>
2014-11-22 12:00:51 -0500

    Delete var id because it was not used

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#input').on('keyup', function(event){
     var keycode = (event.keyCode ? event.keyCode : event.which);
     var text = $('#input').val();
-    var id = "";
+
     if(keycode == '13'){
       $('#input').val("");
         
@@ -14,7 +14,6 @@ $(document).ready(function() {
           var message = $('<div><li id=' + data.id + '><input type="checkbox"> ' + text + '</li></div>');
 
           $('.todo-list').append(message);
-          id = data.id;
         }
       });
     }
