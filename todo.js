ade984969af60c3aafd7c3aaabe9b3af21d72228
Christophe E <cestanol@gmail.com>
2014-11-22 12:05:23 -0500

    Copy and paste GET from challenges

--- todo.js
+++ todo.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
 
     if(keycode == '13'){
       $('#input').val("");
-        
+
       $.ajax("http://makeitreal-todo.herokuapp.com/todo_items", {
         type: 'POST',
-        contentType: "application/json", 
+        contentType: "application/json",
         data: JSON.stringify({ title: text }),
         success: function(data) {
-          var message = $('<div><li id=' + data.id + '><input type="checkbox"> ' + text + '</li></div>');
+          var message = $('<div><li id=' + data.id + '><input type="checkbox"><label for="task"> ' + text + '</label></li></div>');
 
           $('.todo-list').append(message);
         }
@@ -21,7 +21,7 @@ $(document).ready(function() {
   });
 
 
-$('.todo-list').ON('click', '.CheckBox_class', function(){
+$('.todo-list').on('click', '.CheckBox_class', function(){
 	var li = $(this).closest("li");
   	var itemId = li.attr("id");
   	var url = "http://makeitreal-todo.herokuapp.com/todo_items/"+itemId;
@@ -42,3 +42,22 @@ $('.todo-list').ON('click', '.CheckBox_class', function(){
   	}
 
 });
+
+
+
+// GET request
+      var GETurl = 'http://makeitreal-todo.herokuapp.com/todo_items';
+      $.get( GETurl, function(result) {
+        $.each(result, function (index, list){
+        if (list.done) {
+          var listElement = ('<li class="task underlined" id="' + list.id + '"><input type="checkbox" name="list" value="list" checked><label for="task">' + list.title + '</label><a href=""><span class="close">' + 'X' + '</span></a></li>');
+        } else {
+          var listElement = ('<li class="task" id="' + list.id + '"><input type="checkbox" name="list" value="list"><label for="task">' + list.title + '</label><a href=""><span class="close">' + 'X' + '</span></a></li>');
+        }
+
+        $(listElement).appendTo('.todo-list');
+        }) // end each
+      })
+
+});
+
